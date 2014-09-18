a577edb31808a17f93c1ac446de1ea3749f56136
jose-teclalabs <j.pulidomurgagithub@gmail.com>
2014-09-17 19:06:25 -0500

    ver django44a

---
+++ asasa/demo/demo/apps/home/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url, include
+
+
+urlpatterns = patterns('demo.apps.home.views',
+	url(r'^$','index_view',name='vista_principal'),
+	url(r'^about/$','about_view',name='vista_about'),
+	url(r'^productos/page/(?P<pagina>.*)/$','productos_view',name='vista_productos'),
+	url(r'^producto/(?P<id_prod>.*)/$','singleProduct_view',name='vista_single_producto'),
+	url(r'^contacto/$','contacto_view',name='vista_contacto'),
+	url(r'^login/$','login_view',name='vista_login'),
+	url(r'^registro/$','register_view',name='vista_registro'),
+	url(r'^logout/$','logout_view',name='vista_logout'),
+)
